19524a9d1979013d9ab7c4e5563805b4e1fd3ff4
khadamati <khadamati.sharafat@gmail.com>
2022-11-06 16:57:21 +0330

    add models to admin page part1

--- poses/admin.py
+++ poses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PosRegister, Login
+
+@admin.register(PosRegister)
+class PosRegisterAdmin(admin.ModelAdmin):
+    list_display = ['id','first_name', 'last_name','national_code' , 'created_time', 'updated_time']
+    list_filter = ['id', 'first_name', 'last_name', 'national_code']
+    search_fields = ['id', 'first_name', 'last_name', 'national_code']
+
+.3.....
